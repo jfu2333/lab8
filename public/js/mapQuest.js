e67d5d14acd856422b54218b9cc2a743dc4049d0
Jiayu Fu <j3fu@ucsd.edu>
2020-02-27 00:17:29 -0800

    .

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+L.mapquest.key = '0rj4v75MQDfrfVmdHnsqwcQzcOyuRDqE';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.882703, -117.235757],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+
+L.marker([32.882703, -117.235757]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -6,5 +6,13 @@ var data = {
  * GET tasks page.
  */
 exports.adminView = function(request, response){
-  response.render('rsvp', data);
+	response.render('rsvp',data);
+}
+exports.addRSVP = function(request, response){
+	var rsvpEmail = request.body.rsvpEmail;
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail);
+
+	response.send(rsvpEmail);
 };
